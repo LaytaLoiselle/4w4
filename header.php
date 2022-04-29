ede254beb2fce0866b1ab6885cae965b952ff903
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-04-29 13:10:34 -0400

    Initialisation tp2

---
+++ header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * L'entête « header » de chacune des pages de notre site
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head() ?>
+</head>
+<body  <?php body_class("site"); ?>>
+<header class="site__header" style="background-color: <?= get_theme_mod("couleur_background_body");?>;">
+    <section class="site__header__titre">
+    <?php echo get_custom_logo(); ?>
+    <h1 class="header__titre">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <?php bloginfo('name'); ?></a>
+    </h1>
+    <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+    </section>
+    <section class="util">
+        <div class="util_lien">
+        <?php get_sidebar( 'entete_1' ); ?>
+        </div>
+        <?php get_search_form(); ?>
+    </section>
+</header>
+<section class="site__barre">
+    <input type="checkbox"  id="chk-burger">
+    <label for="chk-burger" id="burger">
+        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="white"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </label>
+    <?php wp_nav_menu(array("menu"=>"principal",
+                            "container"=>"nav")); ?>
+</section>
---
+++ readme.md
@@ -0,0 +1,10 @@
+# ex4 - 4w4 - Conception d'interface et développement Web
+### Auteur : **Isaac Loiselle**
+### Date de remise : 26 avril 2022
+### Description: Voici le thème de l'exercice 4 en 4w4
+
+```
+
+
+Pour modifier readme.md
+https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
