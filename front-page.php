ab5871f22b6640f167e886c5f9f14603cc0229b6
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-05-05 09:10:13 -0400

    Creation du fichier  galerie.scss dans le document composantes

--- front-page.php
+++ front-page.php
@@ -19,11 +19,10 @@
     wp_nav_menu(array("menu"=>"evenement",
                     "container"=>"nav"));
 ?>          
-     <?php if (have_posts()): while(have_posts()) : the_post(); ?>
-        <?php the_title() ?>
-        <?php the_content() ?>
-        <?php endwhile; ?>
-     <?php endif ?>
+   <?php if (have_posts()): the_post(); ?>
+        <h1><?php the_title() ?></h1>
+        <?php the_content() ?>   
+   <?php endif ?>
 </main>
 </div>
 <?php get_footer() ?>
