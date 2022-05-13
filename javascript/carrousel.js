e4044ea1f68fa1d420c82a990412096835b1f470
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-05-13 15:47:25 -0400

    avancement de la section boite_carrousel__img

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -2,21 +2,35 @@
     let boite__carrousel = document.querySelector('.boite__carrousel')
     let boite__carrousel__ferme = document.querySelector('.boite__carrousel__ferme')
     let boite__carrousel__nav = document.querySelector('.boite__carrousel__nav')
+    let boite__carrousel__img = document.querySelector('.boite__carrousel__img')
 
     let galerie__img = document.querySelectorAll('.galerie img')
     console.log(galerie__img.length)
 
-    let elmImg = document.createElement('img')
-    boite__carrousel.append(elmImg)
 
 
-    
+    /* on parcour chacune des images dans la galerie*/
     let index = 0;
     for (const img of galerie__img){
-        let bouton = document.createElement('button')
+
+         /* insertion des radios bouton dans boite__carrousel__navigation*/
+        let bouton = document.createElement('input')
+        bouton.setAttribute('type','radio')
+        bouton.setAttribute9('checked','')
+        bouton.setAttribute9('name','bouton')
+        bouton.setAttribute9('class','bouton')
         bouton.dataset.index = index++
         boite__carrousel__nav.append(bouton)
+
+
+        /* insertion des elmImg de la galerie_img dans boite__carrouselle_img */
+        
+        let elmImg = document.createElement('img')
+        elmImg.img.setAttribute('src',img.getAttribute('src'))
+        boite__carrousel__img.append(elmImg)
+
         bouton.addEventListener('mousedown', function(){
+
             elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
         })
 
