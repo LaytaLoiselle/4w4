aa5eaaa373b95ee5aafea6956a2384f530cfb46b
IsaacLoiselle <100873727+IsaacLoiselle@users.noreply.github.com>
2022-05-13 16:47:26 -0400

    Retour à l'ancien carrousel, car impossible de faire fonctionner le nouveau code et ajout d'un lien github page dans le readme

--- javascript/carrousel.js
+++ javascript/carrousel.js
@@ -1,50 +1,36 @@
 (function(){
-    let boite__carrousel = document.querySelector('.boite__carrousel')
-    let boite__carrousel__ferme = document.querySelector('.boite__carrousel__ferme')
-    let boite__carrousel__nav = document.querySelector('.boite__carrousel__nav')
-    let boite__carrousel__img = document.querySelector('.boite__carrousel__img')
+  let boite__carrousel = document.querySelector('.boite__carrousel')
+  let boite__carrousel__ferme = document.querySelector('.boite__carrousel__ferme')
+  let boite__carrousel__nav = document.querySelector('.boite__carrousel__nav')
 
-    let galerie__img = document.querySelectorAll('.galerie img')
-    console.log(galerie__img.length)
+  let galerie__img = document.querySelectorAll('.galerie img')
+  console.log(galerie__img.length)
 
-
-
-    /* on parcour chacune des images dans la galerie*/
-    let index = 0;
-    for (const img of galerie__img){
-
-         /* insertion des radios bouton dans boite__carrousel__navigation*/
-        let bouton = document.createElement('input')
-        bouton.setAttribute('type','radio')
-        bouton.setAttribute9('checked','')
-        bouton.setAttribute9('name','bouton')
-        bouton.setAttribute9('class','bouton')
-        bouton.dataset.index = index++
-        boite__carrousel__nav.append(bouton)
+  let elmImg = document.createElement('img')
+  boite__carrousel.append(elmImg)
 
 
-        /* insertion des elmImg de la galerie_img dans boite__carrouselle_img */
-        
-        let elmImg = document.createElement('img')
-        elmImg.img.setAttribute('src',img.getAttribute('src'))
-        boite__carrousel__img.append(elmImg)
+  
+  let index = 0;
+  for (const img of galerie__img){
+      let bouton = document.createElement('button')
+      bouton.dataset.index = index++
+      boite__carrousel__nav.append(bouton)
+      bouton.addEventListener('mousedown', function(){
+          elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
+      })
 
-        bouton.addEventListener('mousedown', function(){
+      img.addEventListener('mousedown',function(){
+          console.log(this.tagName)
+          boite__carrousel.classList.add('boite__carrousel__ouvrir')
+          //console.log(this.getAttribute('src'))
+          elmImg.setAttribute('src', this.getAttribute('src'))
+      })
 
-            elmImg.setAttribute('src', galerie__img[this.dataset.index].getAttribute('src'))
-        })
+  }
 
-        img.addEventListener('mousedown',function(){
-            console.log(this.tagName)
-            boite__carrousel.classList.add('boite__carrousel__ouvrir')
-            //console.log(this.getAttribute('src'))
-            elmImg.setAttribute('src', this.getAttribute('src'))
-        })
-
-    }
-
-    boite__carrousel__ferme.addEventListener('mousedown', function(){
-        boite__carrousel.classList.remove('boite__carrousel__ouvrir')
-    })
+  boite__carrousel__ferme.addEventListener('mousedown', function(){
+      boite__carrousel.classList.remove('boite__carrousel__ouvrir')
+  })
 
 })()
--- readme.md
+++ readme.md
@@ -8,3 +8,5 @@
 
 Pour modifier readme.md
 https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+La Githib page: https://laytaloiselle.github.io/4w4/
